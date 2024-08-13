ec4f41df4e53457ee6b9962dbcd0e39b086d4867
Andre Prayogo <andreprayoga187@gmail.com>
2024-08-14 00:44:38 +0700

    installation hardhat

---
+++ hardhat.config.js
@@ -0,0 +1,16 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+import("hardhat/config").HardhatUserConfig;
+
+const PRIVATE_KEY = vars.get("PRIVATE_KEY");
+
+module.exports = {
+  defaultNetwork: "swisstronik",
+  solidity: "0.8.19",
+  networks: {
+    swisstronik: {
+      url: "https://json-rpc.testnet.swisstronik.com/",
+      accounts: [`0x` + `${PRIVATE_KEY}`],
+    },
+  },
+};
